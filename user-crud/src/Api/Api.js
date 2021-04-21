ed6ac37a9d7b394043946303eeac2c91ff8f72f7
lissms <felissms@gmail.com>
2021-04-21 08:41:02 +0200

    create functions to get and send data from api

---
+++ user-crud/src/Api/Api.js
@@ -0,0 +1,58 @@
+//GET
+
+export const getUser = () => {
+  return fetch("http://hello-world.innocv.com/api/user")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
+
+export const getUserDetails = (id) => {
+  return fetch(`http://hello-world.innocv.com/api/user/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return {
+        name: data.name,
+        birthdate: data.birthdate,
+      };
+    });
+};
+
+//POST
+
+export const addNewUser = (name, birthdate) => {
+  return fetch(`http://hello-world.innocv.com/api/user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, birthdate }),
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+};
+
+//PUT
+
+export const updateUser = (id, name, birthdate) => {
+  return fetch(`http://hello-world.innocv.com/api/user`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, name, birthdate }),
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+};
+
+//DELETE
+
+export const deleteUser = (id) => {
+  return fetch(`http://hello-world.innocv.com/api/user/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+};
